bf7619c69e5bffa4bc581b6b9dc691d8a528cafc
Md Eesha <md.eesha@chicmic.co.in>
2022-01-03 15:06:11 +0530

    ...changes in UserToken and Challenge History

--- app/models/sessionModel.js
+++ app/models/sessionModel.js
@@ -3,17 +3,17 @@
 /************ Modules **********/
 const MONGOOSE = require('mongoose');
 const Schema = MONGOOSE.Schema;
-const { TOKEN_TYPES, USER_TYPE, DEVICE_TYPES } = require("../utils/constants");
+const { TOKEN_TYPES, USER_TYPES, DEVICE_TYPES } = require("../utils/constants");
 
 /************ user Session Model **********/
 const sessionSchema = new Schema({
     userId: { type: Schema.Types.ObjectId, ref: 'users' },
-   // userType: { type: Number, enum: Object.values(USER_TYPE), default: USER_TYPE.USER },
+    userType: { type: Number, enum: Object.values(USER_TYPES), default: USER_TYPES.USER },
    // tokenType: { type: Number, default: TOKEN_TYPES.LOGIN },
     token: { type: String },
   //  tokenExpDate: { type: Date },
    // deviceType: { type: Number, enum: Object.values(DEVICE_TYPES) },
-   // deviceToken: { type: String },
+    deviceToken: { type: String },
    // data: { type: Object }
 }, { timestamps: true, versionKey: false });
 
---
+++ config/firebaseAccountKey.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "type": process.env.FCM_ACCOUNT_TYPE || "",
+    "project_id": process.env.FCM_PROJECT_ID || "",
+    "private_key_id": process.env.FCM_PRIVATE_KEY_ID || "",
+    "private_key": process.env.FCM_PRIVATE_KEY.replace(/\\n/g, '\n') || "",
+    "client_email": process.env.FCM_CLIENT_EMAIL || "",
+    "client_id": process.env.FCM_CLIENT_ID || "",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": process.env.FCM_AUTH_PROVIDER_X509_CERT_URL || "",
+    "client_x509_cert_url": process.env.FCM_CLIENT_X509_CERT_URL || "",
+}
