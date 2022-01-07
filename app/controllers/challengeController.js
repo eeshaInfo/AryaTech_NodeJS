5797795b72e244b59f4f9e8d54e8b3ae40583c04
HimanshuRanaChicmic <himanshu.rana@chicmic.co.in>
2022-01-07 19:56:08 +0530

    --minor changes

--- app/controllers/challengeController.js
+++ app/controllers/challengeController.js
@@ -183,7 +183,7 @@ challengeController.getChallengesByUser = async (payload) => {
   }
   // get all challenge by particular user
   let list = await SERVICES.challengeService.getChallengesByUser(payload, { skip: payload.skip, limit: payload.limit });
-  let totalCounts = list[0].totalCount
+  let totalCounts = list.length ? list[0].totalCount : 0;
   //let totalCounts = await SERVICES.challengeService.getUserCountByChallenge(criteria);
   return Object.assign(HELPERS.responseHelper.createSuccessResponse(MESSAGES.CHALLENGE_FETCHED_SUCCESSFULLY), { data: { list, totalCounts } });
 };
